a8f0fea06632430ebf4144c972872dcb5fd7d4a8
cloudrumbles <shahriyyah@gmail.com>
2025-01-24 17:12:51 +0000

    modify default timezone to be singapore

--- letta/utils.py
+++ letta/utils.py
@@ -851,7 +851,7 @@ def get_local_time_military():
     return formatted_time
 
 
-def get_local_time_timezone(timezone="America/Los_Angeles"):
+def get_local_time_timezone(timezone="Asia/Singapore"):
     # Get the current time in UTC
     current_time_utc = datetime.now(pytz.utc)
 
